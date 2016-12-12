cf8dd1ca66ecd412db4f1b47ec304336cfd166cf
amin-salarkia <salarkiama@ce.sharif.ir>
2016-12-12 15:49:32 +0330

    I added isPrime

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int isPrime(int a){
+	int i=0;
+	if(a<=1)
+		return 0;
+	for(i=2;i*i<=a;i++){
+		if(a%i==0)
+			return 0;
+	}
+	return 1;
+}
+
 int main(){
 	printf ("Hello Git!\n");
 	printf ("Bye\n");
